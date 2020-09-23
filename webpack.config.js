1338f87b903fc6fdf197618ae9ea3b929a34f878
linxueqin <920739257@qq.com>
2020-09-23 22:50:07 +0800

    '构建'

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'js/app.js'
+  },
+  module: {
+    rules: [{
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            name: 'resource/[name].[ext]'
+          },
+        }],
+      },
+      {
+        test: /\.(woff|svg|woff2|eot|ttf|otf)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 1000000,
+            name: 'resource/[name].[ext]'
+          },
+        }]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new ExtractTextPlugin("css/[name].css"),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'commin',
+      filename: 'js/base.js'
+    })
+  ],
+  devServer: {
+    port: 8080,
+  }
+};
